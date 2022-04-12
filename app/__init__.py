2c5c83527f072c8675e1e6e0dcf55b7a321b1dfd
yukiao <lembang19sony@gmail.com>
2022-04-12 18:48:50 +0800

    add heroku deployment files

--- app/__init__.py
+++ app/__init__.py
@@ -1,19 +1,22 @@
-
+import os
+from dotenv import load_dotenv
 from flask import Flask
 from flask_ckeditor import CKEditor
 from flask_wtf.csrf import CSRFProtect
 from flask_bcrypt import Bcrypt
 from flask_mongoengine import MongoEngine
 
+if os.environ.get('FLASK_ENV') == 'development':
+    load_dotenv()
+
 app = Flask(__name__)
 
-app.config['SECRET_KEY'] = "VERY SECRET"
+app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 app.config['CKEDITOR_FILE_UPLOADER'] = "upload"
 app.config['CKEDITOR_ENABLE_CSRF'] = True
 app.config['MONGODB_SETTINGS'] = {
     'db': 'blog',
-    'host': '127.0.0.1',
-    'port': 27017
+    'host': os.environ.get('MONGO_DB_URI'),
 }
 
 csrf = CSRFProtect(app)
